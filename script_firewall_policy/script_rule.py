a071b28a9ced84bcc4ae248879df66fd76b295c3
Anderson Porfirio <anderson.porfirio@SA12461.local>
2017-12-24 17:20:41 -0200

    Sys.Arg function

--- script_firewall_policy/script_rule.py
+++ script_firewall_policy/script_rule.py
@@ -1,33 +1,45 @@
 import os
 import sys
 
-if len(sys.argv) < 3:
-    print("Usage: script.py vdom rules.txt")
-    exit()
+def config_firewall_policy():
+	commands = ["set logtraffic all","set logtraffic utm",
+            	"set logtraffic disable","set status disable",
+            	"set status enable"]
 
-vdom = sys.argv[1]
+	if len(sys.argv) < 4:
+		print("Usage: script.py vdom rules.txt command")
+	exit()
 
-with open(sys.argv[2]) as rules_file:
-    list01 = rules_file.readlines()
+	vdom = sys.argv[1]
+	command = sys.argv[3]
 
-rules = []
-disable_log = []
+	if command not in commands:
+		print("Command not allowed")
+	exit()
 
-for line in list01:
-	rules.append(line.strip())
+	with open(sys.argv[2]) as rules_file:
+		list01 = rules_file.readlines()
 
-disable_log.append('config vdom\nedit %s\nconfig firewall policy' %vdom)
-for line in rules:
-	edit = ('edit %s\nset logtraffic disable\nnext' %line)
-	disable_log.append(edit)
-disable_log.append('end')
+	rules = []
+	script = []
+
+	for line in list01:
+		rules.append(line.strip())
+
+	script.append('config vdom\nedit %s\nconfig firewall policy' %vdom)
+	for line in rules:
+		edit = ('edit %s\n%s\nnext' %(line,command))
+		script.append(edit)
+	script.append('end')
 
 
-if os.path.exists('disable_%s.txt' %vdom):
-	os.remove('disable_log_%s.txt' %vdom)
+	if os.path.exists('script_%s.conf' %vdom):
+		os.remove('script_%s.conf' %vdom)
 
-for line in disable_log:
-	with open('disable_log_%s.txt' %vdom, 'a') as cmd:
-		cmd.write(line + '\n')
+	for line in script:
+		with open('script_%s.conf' %vdom, 'a') as cmd:
+			cmd.write(line + '\n')
+
+config_firewall_policy()
 
 

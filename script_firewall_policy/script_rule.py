49b83dba9d72c26cfb280a6fe36f23e74e1d72a5
Anderson Porfirio <anderson.porfirio@SA12461.local>
2017-12-29 11:14:25 -0200

    README.md

--- script_firewall_policy/script_rule.py
+++ script_firewall_policy/script_rule.py
@@ -1,23 +1,32 @@
+from __future__ import absolute_import, division, print_function
+from builtins import input
+import signal
 import os
-import sys
 
-def config_firewall_policy():
-	commands = ["set logtraffic all","set logtraffic utm",
-            	"set logtraffic disable","set status disable",
-            	"set status enable"]
+vdom = input("VDOM name: ")
+file = input("File with id rules name: ")
+print("List of avaliabled commands:\n"
+	  "set logtraffic all\n"
+	  "set logtraffic utm\n"
+	  "set logtraffic disable\n"
+	  "set status disable\n"
+	  "set status enable\n"
+	  )
+command = input("Command to be executabled: ")
 
-	if len(sys.argv) < 4:
-		print("Usage: script.py vdom rules.txt command")
-	exit()
+commands = ["set logtraffic all","set logtraffic utm",
+        	"set logtraffic disable","set status disable",
+        	"set status enable"]
 
-	vdom = sys.argv[1]
-	command = sys.argv[3]
+signal.signal(signal.SIGPIPE, signal.SIG_DFL) #IOError: Broken pine
+signal.signal(signal.SIGINT, signal.SIG_DFL) #KeyboardInterrupt: Ctrl+C
 
+try:
 	if command not in commands:
 		print("Command not allowed")
-	exit()
+		exit()
 
-	with open(sys.argv[2]) as rules_file:
+	with open(file) as rules_file:
 		list01 = rules_file.readlines()
 
 	rules = []
@@ -39,7 +48,6 @@ def config_firewall_policy():
 	for line in script:
 		with open('script_%s.conf' %vdom, 'a') as cmd:
 			cmd.write(line + '\n')
-
-config_firewall_policy()
-
+except Exception as e:
+	print(e)
 
